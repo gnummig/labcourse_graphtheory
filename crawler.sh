d67494adb684c72f7dbc8ea22c55ca970009cd38
Linus <benjaminschindler@bnsn.local>
2018-02-20 13:58:15 +0100

    fixed pythonscript call in crawler.sh

--- crawler.sh
+++ crawler.sh
@@ -5,7 +5,7 @@ echo "GraphID	 VertexCount	Vertex_ID	In_Degree	Out_Degree	ProblemNode	Centrality
 GraphID=0
 while IFS= read -r GRAPH
 do
-	python parse3graphs.py $GRAPH $GraphID
+	python parse3graphs.py $GRAPH -index $GraphID"_"
 	let "GraphID++"
 done < $1
 
--- plot3Graphs.sh
+++ plot3Graphs.sh
@@ -4,5 +4,5 @@ python parse3graphs.py $1 -dot $1
 dot $1"_orig.dot" -Tpdf > $1"_orig.pdf" 
 dot $1_"comp.dot" -Tpdf > $1_"comp.pdf"
 dot $1_"res.dot" -Tpdf > $1_"res.pdf"
-rm $1"_orig.dot" $1_"comp.dot" $1_"res.dot"
+#rm $1"_orig.dot" $1_"comp.dot" $1_"res.dot"
 convert -background none -gravity center  -density 300  $1"_orig.pdf" $1_"comp.pdf" $1_"res.pdf" -append -trim $1".png"
