dc071877550ab8cf586958396e5f666ce6830367
Linus <benjaminschindler@bnsn.local>
2018-02-22 10:36:27 +0100

    fixed header in datatable

--- crawler.sh
+++ crawler.sh
@@ -1,7 +1,7 @@
 #/bin/bash -x 
 
 #find /homes/praktikumserver/praktikum/graph_prak_2018/ -name *.gtf 
-echo "GraphID	GraphKind	VertexCount	ProblemNode	ChimearNode	VertexID	In_Degree	Out_Degree	In_Flow	In_Flow_Std	Out_Flow	Out_Flow_Std	Centrality"
+echo "GraphID	GraphKind	VertexCount	VertexID	ProblemNode	ChimearNode	In_Degree	Out_Degree	In_Flow	In_Flow_Std	Out_Flow	Out_Flow_Std	Centrality"
 GraphID=0
 while IFS= read -r GRAPH
 do
